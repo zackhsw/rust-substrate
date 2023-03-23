8857db3d94a9a68c4cd472bff1d3a8c1177b715f
ZackWong <1073219036@qq.com>
2023-03-24 07:55:43 +0800

    traffic

--- homework4/src/main.rs
+++ homework4/src/main.rs
@@ -1,4 +1,28 @@
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+
+trait Timeable {
+    fn duration(&self) -> u32;
+}
+
+impl Timeable for TrafficLight {
+    fn duration(&self) -> u32 {
+        match *self {
+            TrafficLight::Red => 30,
+            TrafficLight::Yellow => 5,
+            TrafficLight::Green => 45,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
-    println!("Hello, world!");
+    let red = TrafficLight::Red;
+    let yellow = TrafficLight::Yellow;
+    let green = TrafficLight::Green;
+    println!("Red light duration: {} seconds", red.duration());
+    println!("Yellow light duration: {} seconds", yellow.duration());
+    println!("Green light duration: {} seconds", green.duration());
 }
